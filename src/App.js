4849d1205f23b9e4adae93632a261b634dd7cd60
Vinicius Herminio <viniciusherminio1@outlook.com>
2024-02-28 11:06:47 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,131 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <h2>POLÍTICA DE PRIVACIDADE - OXENTE.NET SOLUÇÕES TECNOLÓGICAS EIRELI</h2>
+      <h4>Obrigatório: Aceitação de configurações Dados pessoais</h4>
+      <p>As leis sobre dados pessoais estão mudando no Brasil, e precisamos alertá-los sobre a
+          utilização dos seus dados pessoais feitos pela prestadora</p>
+      <p>Quando você usa nossos serviços, está confiando a nós suas informações. Entendemos que
+          isso é uma grande responsabilidade e trabalhamos duro para proteger essas informações e
+          colocar você no controle.</p>
+      <p>A Política de Privacidade apresenta aos nossos assinantes informações de como coletamos,
+        usamos, armazenamos, tratamos e protegemos os dados pessoais, com absoluta
+        transparência quanto ao assunto e esclarecimentos ao interessado sobre os tipos de dados
+        que são coletados, os motivos da coleta e a forma como os assinantes podem gerenciar e
+        excluir informações. </p>
+      <p>Esta Política de Privacidade se aplica a todos nossos assinantes para a utilização do portal do
+          assinante através do site www.oxente.net e integra aos contratos de prestação de serviços,
+          feitos pela prestadora OXENTE.NET SOLUÇÕES TECNOLÓGICAS EIRELI, devidamente
+          inscrita no CNPJ sob nº 06.338.637/0001-79, com sede na Pça Dom José Lopes, nº 06, Bairro
+          São Sebastião, da cidade de Pesqueira/PE, Prestadora de Serviços de Telecomunicações
+          Multimídia</p>
+        <p>Todas as condições do documentos estão em conformidade com a Lei Geral de Proteção de
+          Dados Pessoais (Lei 13.709/18), o Marco Civil da Internet (Lei 12.965/14) e Código de Defesa
+          do Consumidor (Lei 8078/90). O documento poderá ser atualizado em decorrência de eventual
+          atualização normativa, razão pela qual se convida o assinante a consultar periodicamente esta
+          seção.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h4>Como recolhemos os dados pessoais:</h4>
+      <p>Os dados pessoais do assinante são recolhidos pela plataforma da seguinte forma:</p>
+      <p>Cadastramento do assinante para a criação de um perfil na plataforma Voalle: Esses dados
+        são de identificação básica, como: Nome completo, CPF, endereço, email e telefone. A partir
+        deles, podemos identificar assinante, além de garantir uma maior segurança e bem-estar à
+        suas necessidades. Ficam cientes os assinantes de que seu perfil na plataforma estará
+        acessível aos colaboradores da Oxente.net.</p>
+      <h4>Quais dados pessoais recolhemos sobre o ASSINANTE:</h4>
+      <p>Os dados pessoais do assinante recolhidos são os seguintes:</p>
+      <p>Nome completo, endereço de residência, e-mail e telefone de contato</p>
+      <h4>Pra que finalidades utilizamos os dados pessoais do assinante:</h4>
+      <p>Os dados pessoais dos assinantes coletados e armazenados pela plataforma Voalle tem por
+        finalidade:</p>
+      <p>Dados de contrato: conferir às partes segurança jurídica e facilitar a conclusão do negócio.</p>
+      <p>O tratamento de dados pessoais para finalidades não previstas nesta Política de Privacidade
+        somente ocorrerá mediante comunicação prévia ao assinante, de modo que os direitos e
+        obrigações aqui previstos permanecem aplicáveis.</p>
+      <h4>Por quanto tempo os dados pessoais ficam armazenados:</h4>
+      <p>Os dados pessoais do assinante são armazenados pela plataforma durante o período
+          necessário para a prestação do serviço ou o cumprimento das finalidades previstas no
+          presente documento, conforme o disposto no inciso I do artigo 15 da Lei 13.709/18.</p>
+      <p>Os dados podem ser removidos ou anonimizados a pedido do assinante, excetuando os casos em que a lei oferecer outro tratamento.</p>
+      <p>Ainda, os dados pessoais do assinante apenas podem ser conservados após o término de seu
+        tratamento nas seguintes hipóteses previstas no artigo 16 da referida Lei:</p>
+      <p>I - cumprimento de obrigação legal ou regulatória pelo controlador;<br/>
+        II - estudo por órgão de pesquisa, garantida, sempre que possível, a anonimização dos dados
+        pessoais;<br/>
+        III - transferência a terceiro, desde que respeitados os requisitos de tratamento de dados
+        dispostos nesta Lei; ou<br/>
+        IV - uso exclusivo do controlador, vedado seu acesso por terceiro, e desde que anonimizados
+        os dados.</p>
+      <h4>Segurança dos dados pessoais armazenados:</h4>
+      <p>A plataforma se compromete a aplicar as medidas técnicas e organizativas aptas a proteger os
+        dados pessoais de acessos não autorizados e de situações de destruição, perda, alteração,
+        comunicação ou difusão de tais dados.</p>
+      <p>Os dados relativos a cartão de crédito são criptografados usando a tecnologia “secure socket
+        layer” (SSL) que garante a transmissão dos dados de forma segura e confidencial, de modo
+        que a transmissão dos dados entre o servidor e o assinante ocorre de maneira cifrada e
+        encriptada.</p>
+      <p>A plataforma não se exime de responsabilidade por culpa exclusiva de terceiro, como em caso
+        de ataque de hackers ou crackers, ou culpa exclusiva do assinante, como no caso em que ele
+        mesmo transfere seus dados a terceiros. O site se compromete a comunicar o assinante em
+        caso de alguma violação de segurança dos seus dados pessoais.</p>
+      <p>Os dados pessoais armazenados são tratados com confidencialidade dentro dos limites legais.
+        No entanto, podemos divulgar suas informações pessoais caso sejamos obrigados pela lei pra
+        fazê-lo ou se você violar nosso termos de serviços.</p>
+      <h4>Compartilhamento dos dados:</h4>
+      <p>O compartilhamento de dados do assinante ocorre apenas com os dados referentes a
+        publicações realizadas pelo próprio assinante, tais ações são compartilhadas publicamente
+        com os outros assinantes.
+      </p>
+      <p>Os dados do perfil do assinante são compartilhados publicamente em sistemas de busca e
+        dentro da plataforma, sendo permitido ao assinante modificar tal configuração para que seu
+        perfil não apareça nos resultados de busca de tais ferramentas.</p>
+      <h4>Os dados pessoais armazenados serão transferidos a terceiros:</h4>
+      <p>Os dados pessoais podem ser compartilhados (com terceiros) com as seguintes pessoas
+        (físicas/jurídicas): empresas processadoras de transação de pagamentos, empresas
+        fornecedoras de conteúdos fornecidos aos assinantes, de acordo com o plano contratado
+        como serviços de proteção digital, streaming de vídeos e música.</p>
+        <p>Os terceiros indicados receberão os dados na medida do necessário para permitir que eles
+          realizem os serviços contratados, ou seja, a fim de garantir única e exclusivamente a execução
+          do contrato firmado com a prestadora.</p>
+        <p>Com relação aos fornecedores de serviços terceirizados como processadores de transação de
+          pagamento, informamos que cada qual tem a sua própria política de privacidade. Desse modo,
+          recomendamos a leitura das suas políticas de privacidade para compreensão de quais
+          informações pessoais serão usadas por esses fornecedores.</p>
+        <p>Os fornecedores podem ser localizados ou possuir instalações localizadas em países
+          diferentes. Nessas condições, os dados pessoais transferidos podem se sujeitar às leis de
+          jurisdições nas quais o fornecedor de serviço ou suas instalações estão localizados.</p>
+        <p>Ao acessar nossos serviços e prover suas informações, você, assinante, está consentindo o
+          processamento, transferência e armazenamento desta informação em outros países.</p>
+        <p>Não somos responsáveis pelas práticas de privacidade de outros sites e lhe incentivamos a ler
+            as declarações de privacidades que cada um deles ofertam.</p>
+        <h4>Consentimento:</h4>
+        <p>Ao utilizar os serviços e fornecer as informações pessoais na plataforma, o assinante está
+          consentindo com a presente Política de Privacidade.</p>
+        <p>O assinante, ao cadastrar-se, manifesta conhecer e pode exercitar seus direitos de cancelar
+            seu cadastro, acessar e atualizar seus dados pessoais e garante a veracidade das
+          informações por ele disponibilizadas.</p>
+        <p>O assinante tem direito de retirar o seu consentimento a qualquer tempo, para tanto deve
+          entrar em contato através do e-mail sac@oxente.net ou por correio enviado ao seguinte
+          endereço: Pça Dom José Lopes,06 – Piso 2 – Centro – Pesqueira – PE / 55200-000.</p>
+        <h4>Alterações para essa política de privacidade:</h4>
+        <p>Reservamos o direito de modificar essas Política de Privacidade a qualquer momento, então, é
+          recomendável que o assinante revise-a com frequência.</p>
+        <p>As alterações e esclarecimentos vão surtir efeito imediatamente após sua publicação na
+          plataforma. Quando realizadas alterações os assinantes serão notificados. Ao utilizar o
+          serviços ou fornecer informações pessoais após eventuais modificações, o assinante
+          demonstra concordância com as novas normas.</p>
+        <p>Diante da fusão ou venda da plataforma à outra empresa os dados dos assinantes podem ser
+          transferidas para os novos proprietários para que permanência dos serviços oferecidos.</p>
+        <h4>Jurisdição para resolução de conflitos:</h4>
+        <p>Para a solução de controvésias decorrentes do presente instrumento será aplicado
+          integralmente o direito brasileiro.</p>
+        <p>O presente instrumento obriga herdeiros e/ou sucessores, a qualquer tempo, sendo neste ato
+          eleito pelas partes o foro da comarca da Cidade de Pesqueiro, estado de Pernambuco,
+          competente para dirimir quaisquer questões referentes ao presente, com renúncia expressa de
+          qualquer outro, por mais privilegiado que seja.</p>
+</div>
   );
 }
 
